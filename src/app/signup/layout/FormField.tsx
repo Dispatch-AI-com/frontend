457108689b08964c29354ac81737fd3ec8499a2e
Depeng Sun <shangguming@gmail.com>
2025-04-10 01:28:41 +0930

    font weight fixed

--- src/app/signup/layout/FormField.tsx
+++ src/app/signup/layout/FormField.tsx
@@ -18,7 +18,7 @@ export default function FormField({ label, children, size = "normal", mb = 0 }: 
   return (
     <Box width="100%" minHeight={sizeMap[size]} mb={mb}>
       {label && (
-        <Typography sx={{ fontSize: "16px", fontWeight: 600 }} ml={0.5} mb={0.8}>
+        <Typography sx={{ fontSize: "16px"}} ml={0.5} mb={0.8}>
           {label}
         </Typography>
       )}
