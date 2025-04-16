3a0f74e4ed7385efa04d94ff96ea8ad7aeb535bf
Depeng Sun <depengsun.dev@gmail.com>
2025-04-16 13:59:09 +0000

    update packsize

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracing: false,
-  swcMinify: true,
-  experimental: {
-    turbo:{}
-  },
+  output: "standalone",
 };
 
 export default nextConfig;
