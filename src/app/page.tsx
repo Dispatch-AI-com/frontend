edfb72df94d3d8461e008bf14cbaf2ebcad97093
Michael <markconsulting1871@gmail.com>
2025-04-19 09:26:59 +1000

    Created footer.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@mui/material';
 import HeroSection from '../components/features/landing/HeroSection';
+import Footer from '../components/features/landing/Footer';
 
 
 export default function LandingPage() {
   return (
     <Container>
       <HeroSection />
+      <Footer />
     </Container>
   );
 }
