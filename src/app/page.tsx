799b32183b6c170e191b94a0ec24fd3ea34c3146
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-05-01 09:36:38 +1000

    bugfix-merge-conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-import LandingPage from "./landing/page";
-
-export default function Home() {
-  return <LandingPage />;
-=======
-
 import HeroSection from '@/components/features/landing/HeroSection';
 
 
@@ -14,5 +7,4 @@ export default function Home() {
       <HeroSection />
     </>
   );
->>>>>>> 2a690458f3daf1fdd8a4495bfd7dbe6b6c16b346
 }
