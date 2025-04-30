020949a0fd8f941201ea7164974deb62f5e9dedf
Mark Wang <markconsulting1871@gmail.com>
2025-04-30 10:39:14 +1000

    Added Banner

---
+++ src/app/features/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/features/landing/Navbar';
+import Banner from '@/components/features/features/Banner';
+import Footer from '@/components/features/landing/Footer';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Banner />
+      <Footer />
+    </>
+  );
+}
