3ac23f678a807024bf2057a249078b908bc4d9a2
Mark Wang <markconsulting1871@gmail.com>
2025-04-30 17:04:13 +1000

    Restored page file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Footer from '@/components/features/landing/Footer';
 export default function Home() {
   return (
     <>
-      <Navbar themeMode="dark" />
+      <Navbar />
       <HeroSection />
       <Footer />
     </>
