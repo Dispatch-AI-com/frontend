673c19f93d11360bdfe4f7145638d058c84ec17e
Mark Wang <markconsulting1871@gmail.com>
2025-05-01 16:20:27 +1000

    Created features page layout

---
+++ src/app/features/layout.tsx
@@ -0,0 +1,13 @@
+import FeaturesLayout from "@/components/layout/features-layout";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <FeaturesLayout>
+        {children}
+    </FeaturesLayout>
+  );
+}
---
+++ src/components/layout/features-layout/index.tsx
@@ -0,0 +1,25 @@
+import { Container } from '@mui/material';
+import Navbar from "../main-layout/Navbar"
+import Footer from "../main-layout/Footer"
+
+export default function MainLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+        <>
+            <Container maxWidth="xl">
+              <Navbar />
+            </Container>
+            
+            <Container maxWidth="xl" sx={{py: 4}} >
+                {children}
+            </Container>
+            
+            <Container maxWidth="xl">
+                <Footer />
+            </Container>
+        </>
+    );
+}
