da1f704b90ba90c92561a49bb99910fcff1d32bb
Depeng Sun <shangguming@gmail.com>
2025-05-01 21:35:11 +0930

    ci/update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Landing from "./landing/page";
 import MainLayout from "@/components/layout/main-layout";
+
+import Landing from "./landing/page";
 
 
 export default function Home() {
   return (
     <MainLayout>
       <Landing />
+      <h1>test</h1>
     </MainLayout>
   );
 }
