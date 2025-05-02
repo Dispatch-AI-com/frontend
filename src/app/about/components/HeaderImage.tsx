6b02bb82abc117338f34f21e7d70cc219222f3b0
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-05-02 10:10:02 +1000

    bugfix-image-responsiveness

--- src/app/about/components/HeaderImage.tsx
+++ src/app/about/components/HeaderImage.tsx
@@ -2,7 +2,8 @@ import { styled } from '@mui/material/styles';
 
 export const HeaderImage = styled('img')(({ theme }) => ({
     borderRadius: theme.shape.borderRadius,
-    width: '40%',
+    width: '100%',
+    maxWidth: '576px',
     height: 'auto',
-    objectFit: 'cover',
+    objectFit: 'cover'
 }));
