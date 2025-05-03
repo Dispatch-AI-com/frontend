109f2a72ab77a7632a2256e5e277594de3d7a7ca
Tim Tian <timtian@Mac.modem>
2025-05-04 01:14:47 +0930

    Fixed error

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,10 +1,15 @@
 
 import HeroSection from '@/app/landing/components/HeroSection';
+import TestimonialsSection from './components/TestimonialsSection';
 
 export default function Landing() {
   return (
     <>
       <HeroSection />
+      <TestimonialsSection />
     </>
   );
 }
+
+
+
