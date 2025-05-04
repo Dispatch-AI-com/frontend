bba9b98639d1bc16fadfb2ff37258b28011b88b2
Whiskey-Taste <146010335+Whiskey-Taste@users.noreply.github.com>
2025-05-04 17:55:24 +0930

    Dis 31 landing page comments section (#47)
    * Fixed error
    
    * Fixed error and responsive
    
    * Fixed error and responsive
    
    * Fix ESLint warning
    
    * Fix ESLint warning
    
    * fix: sorted imports in test/page.tsx to pass ESLint
    
    ---------
    
    Co-authored-by: Tim Tian <timtian@Mac.modem>

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,10 +1,14 @@
-
 import HeroSection from '@/app/landing/components/HeroSection';
+import TestimonialsSection from '@/app/landing/components/TestimonialsSection';
 
 export default function Landing() {
   return (
     <>
       <HeroSection />
+      <TestimonialsSection />
     </>
   );
 }
+
+
+
