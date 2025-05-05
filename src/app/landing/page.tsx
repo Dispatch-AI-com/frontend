326a76f17c3a6f4024d768d083caf82f240ac08e
huiii421 <hqh421@gmail.com>
2025-05-05 17:29:33 +1000

    fix import sort

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from '@/app/landing/components/HeroSection';
-import ProcessFlow from "./components/ProcessFlow";
 import TestimonialsSection from '@/app/landing/components/TestimonialsSection';
+
+import ProcessFlow from "./components/ProcessFlow";
 
 export default function Landing() {
   return (
