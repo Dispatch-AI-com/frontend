d5e416f39b4367ece1ae7665f1b52331a4f0b206
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-05-16 12:56:43 +1000

    bugfix/header-container

--- src/components/layout/aboutus-layout/Banner/AboutBannerSection.tsx
+++ src/components/layout/aboutus-layout/Banner/AboutBannerSection.tsx
@@ -3,21 +3,23 @@
 import { Box } from '@mui/material';
 import React from 'react';
 
-import { AboutHeader, HeaderImage, HeaderText, HeaderTitle } from '@/app/about/components/AboutBanner';
+import { AboutHeader, HeaderContainer,HeaderImage, HeaderText, HeaderTitle } from '@/app/about/components/AboutBanner';
 
 const AboutBannerSection = () => {
     return (
-        <AboutHeader>
-            <HeaderImage src="/voice-ai.png" alt="Voice AI" />
-            <Box>
-                <HeaderTitle>
-                    Building the future of <br /> Voice AI
-                </HeaderTitle>
-                <HeaderText>
-                    Transforming human-machine interaction through advanced voice technology.
-                </HeaderText>
-            </Box>
-        </AboutHeader>
+            <AboutHeader>
+                <HeaderContainer>
+                    <HeaderImage src="/voice-ai.png" alt="Voice AI" />
+                    <Box>
+                        <HeaderTitle>
+                            Building the future of <br /> Voice AI
+                        </HeaderTitle>
+                        <HeaderText>
+                            Transforming human-machine interaction through advanced voice technology.
+                        </HeaderText>
+                    </Box>
+                </HeaderContainer>
+            </AboutHeader>
     );
 };
 
