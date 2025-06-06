c236899a91636013de174ffbe7b74d6e98ce658c
Mark Wang <markconsulting1871@gmail.com>
2025-06-06 20:04:54 +1000

    project: './tsconfig.json'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,7 @@ export default tseslint.config(
       },
       parser,
       parserOptions: {
-        projectService: true,
+        project: './tsconfig.json',
         tsconfigRootDir: import.meta.dirname,
         ecmaVersion: 2022,
         sourceType: "module",
