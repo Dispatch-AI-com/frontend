ac641ff5c4c30719f35a024eae06d5642603d767
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-06-16 21:38:38 +1000

    update-all-admin-layout

--- src/app/admin/overview/page.tsx
+++ src/app/admin/overview/page.tsx
@@ -1,15 +1,3 @@
-import { Box } from '@mui/material';
-
-import Sidebar from '@/components/layout/dashboard-layout/Sidebar';
-
 export default function OverviewPage() {
-  return (
-    <Box display="flex">
-      <Sidebar />
-      {/* Main content area can be added here */}
-      <Box flex={1} p={3}>
-        {/* Add your dashboard content here */}
-      </Box>
-    </Box>
-  );
+  return <>{/* Add your main content here */}</>;
 }
