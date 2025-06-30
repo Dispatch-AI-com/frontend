0b415e611420102cd3b4ee17c74be5645de9538a
mark wang <markmagicwang@gmail.com>
2025-06-30 13:28:39 +1000

    Used ready instead of mounted for states after login

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -9,7 +9,9 @@ import { useAppSelector } from '@/redux/hooks';
 
 export default function ProtectedLayout({ children }: { children: ReactNode }) {
   const [mounted, setMounted] = useState(false);
+  const [ready, setReady] = useState(false);
   const token = useAppSelector(s => s.auth.token);
+  const user = useAppSelector(s => s.auth.user);
   const router = useRouter();
   const pathname = usePathname();
 
@@ -18,10 +20,17 @@ export default function ProtectedLayout({ children }: { children: ReactNode }) {
   }, []);
 
   useEffect(() => {
-    if (mounted && !token) {
+    if (mounted) {
+      const isReady = !!token && !!user;
+      setReady(isReady);
+    }
+  }, [mounted, token, user]);
+
+  useEffect(() => {
+    if (ready && !token) {
       router.replace(`/login`);
     }
-  }, [mounted, token, pathname, router]);
+  }, [ready, token, pathname, router]);
 
   if (!mounted) {
     return (
@@ -33,6 +42,22 @@ export default function ProtectedLayout({ children }: { children: ReactNode }) {
         sx={{ visibility: 'hidden' }}
       >
         Loading...
+      </Box>
+    );
+  }
+
+  if (!ready) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Box textAlign="center">
+          <Box mb={2}>Initializing Admin Panel...</Box>
+          <Box>Loading user data and permissions...</Box>
+        </Box>
       </Box>
     );
   }
