35489799c2ec49b5b0a4d14b06f1136ce44828f5
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-08-07 17:20:27 +1000

    delete-redundant-dividing-line

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -1,12 +1,10 @@
 'use client';
-import SectionDivider from '@/app/admin/settings/components/SectionDivider';
 import SettingsSection from '@/app/admin/settings/SettingsSection';
 import { AdminPageLayout } from '@/components/layout/admin-layout';
 
 export default function SettingsPage() {
   return (
     <AdminPageLayout title="Settings" padding="normal" background="solid">
-      <SectionDivider my={1} />
       <SettingsSection />
     </AdminPageLayout>
   );
